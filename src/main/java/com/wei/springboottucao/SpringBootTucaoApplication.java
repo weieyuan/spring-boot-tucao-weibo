bb8852c7db01281c2304d987dde3a971e3fbdc3b
weieyuan <weieyuan@yeah.net>
2017-12-05 11:32:17 +0800

    初始化项目

---
+++ src/main/java/com/wei/springboottucao/SpringBootTucaoApplication.java
@@ -0,0 +1,12 @@
+package com.wei.springboottucao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTucaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTucaoApplication.class, args);
+	}
+}
